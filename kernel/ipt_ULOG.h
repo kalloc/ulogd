e0968bf146e472e88c6b5a3d96643ad19321435c
laforge <laforge>
2000-07-31 11:38:06 +0000

    *** empty log message ***

--- kernel/ipt_ULOG.h
+++ kernel/ipt_ULOG.h
@@ -1,22 +1,13 @@
 #ifndef _IPT_ULOG_H
 #define _IPT_ULOG_H
 
-#ifdef __KERNEL__
-#include <linux/netdevice.h>
-#endif
-
 #define ULOG_MAC_LEN	80
-
-
-/* just until this is in netfilter.h */
-#ifndef NETLINK_NFLOG
-#define NETLINK_NFLOG 25
-#endif
+#define ULOG_PREFIX_LEN	32
 
 struct ipt_ulog_info {
 	unsigned char logflags;
 	unsigned int nl_group;
-	char prefix[30];
+	char prefix[ULOG_PREFIX_LEN];
 };
 
 typedef struct ulog_packet_msg {
@@ -27,7 +18,7 @@ typedef struct ulog_packet_msg {
 	char indev_name[IFNAMSIZ];
 	char outdev_name[IFNAMSIZ];
 	size_t data_len;
-	char prefix[30];
+	char prefix[ULOG_PREFIX_LEN];
 	unsigned char mac_len;
 	unsigned char mac[ULOG_MAC_LEN];
 	unsigned char payload[0];
